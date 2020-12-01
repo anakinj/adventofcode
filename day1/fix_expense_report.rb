7607a05e0f0bcfa9e1ca59b8e2ef4120656ca006
Joakim Antman <antmanj@gmail.com>
2020-12-01 11:07:40 +0200

    Day 1 part 2

--- day1/fix_expense_report.rb
+++ day1/fix_expense_report.rb
@@ -12,3 +12,18 @@ the_numbers = numbers.each_with_object([]) do |first_number, arr|
 end.uniq
 
 puts the_numbers.first * the_numbers.last if the_numbers.size == 2
+
+the_numbers = numbers.each_with_object([]) do |first_number, arr|
+  numbers.each do |second_number|
+    numbers.each do |third_number|
+      if (first_number + second_number + third_number) == 2020
+        arr << first_number
+        arr << second_number
+        arr << third_number
+        break
+      end
+    end
+  end
+end.uniq
+
+puts the_numbers[0] * the_numbers[1] *the_numbers[2] if the_numbers.size == 3
