f6cd9c8b1c2e8035fe5e9fd61a6e88318c650b43
Joakim Antman <antmanj@gmail.com>
2020-12-03 08:20:54 +0200

    Day 3 part 2

--- lib/day3/toboggan_trajectory.rb
+++ lib/day3/toboggan_trajectory.rb
@@ -1,15 +1,26 @@
 # frozen_string_literal: true
 
 module TobogganTrajectory
-  def self.count_trees(input)
+  def self.count_trees(input, down: 1, right: 3)
     track = input.each_line.map(&:chomp)
 
     index = 0
-    track.count do |row|
+    tree_count = 0
+    track.each_with_index do |row, distance|
+      next if distance % down != 0
+
       index -= row.size if index >= row.size
-      (row[index] == '#').tap do
-        index += 3
-      end
+      tree_count += 1 if row[index] == '#'
+      index += right
     end
+    tree_count
+  end
+
+  def self.count_all_slopes(input)
+    [count_trees(input, right: 1, down: 1),
+     count_trees(input, right: 3, down: 1),
+     count_trees(input, right: 5, down: 1),
+     count_trees(input, right: 7, down: 1),
+     count_trees(input, right: 1, down: 2)].inject(:*)
   end
 end
--- spec/day3/toboggan_trajectory_spec.rb
+++ spec/day3/toboggan_trajectory_spec.rb
@@ -1,19 +1,24 @@
 # frozen_string_literal: true
 
 RSpec.describe TobogganTrajectory do
-  describe '.count_threes' do
-    context 'when short track is used' do
-      let(:input) { File.read(File.join(__dir__, 'input1')) }
-      it 'detects 2 valid passwords' do
-        expect(described_class.count_trees(input)).to eq(7)
-      end
+  context 'when short track is used' do
+    let(:input) { File.read(File.join(__dir__, 'input1')) }
+    it 'counts 7 trees' do
+      expect(described_class.count_trees(input)).to eq(7)
     end
 
-    context 'when the long list is given' do
-      let(:input) { File.read(File.join(__dir__, 'input2')) }
-      it 'detects the valid passwords' do
-        expect(described_class.count_trees(input)).to eq(223)
-      end
+    it 'calculates 336' do
+      expect(described_class.count_all_slopes(input)).to eq(336)
+    end
+  end
+
+  context 'when the long list is given' do
+    let(:input) { File.read(File.join(__dir__, 'input2')) }
+    it 'counts all the threes' do
+      expect(described_class.count_trees(input)).to eq(223)
+    end
+    it 'comes up with a number' do
+      expect(described_class.count_all_slopes(input)).to eq(3_517_401_300)
     end
   end
 end
