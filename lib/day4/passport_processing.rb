12e786ac0c13b0b010f19be08deaf27ab167a6f3
Joakim Antman <antmanj@gmail.com>
2020-12-04 21:21:26 +0200

    Day 4 Part 2

--- lib/day4/passport_processing.rb
+++ lib/day4/passport_processing.rb
@@ -1,20 +1,50 @@
 # frozen_string_literal: true
 
 module PassportProcessing
-  def self.count_valid(input)
-    passports = input.split(/^$/).map do |passport_section|
+  REQUIRED_FIELDS = %w[ecl pid eyr hcl byr iyr hgt].freeze
+
+  ECL_VALUES = %w[amb blu brn gry grn hzl oth].freeze
+
+  HGT_VALUES = {
+    'cm' => 150..193,
+    'in' => 59..76
+  }.freeze
+
+  VALIDATORS = {
+    'byr' => ->(v) { (1920..2002).include?(v.to_i) },
+    'iyr' => ->(v) { (2010..2020).include?(v.to_i) },
+    'eyr' => ->(v) { (2020..2030).include?(v.to_i) },
+    'hgt' => lambda { |v|
+      (m = /^(?<height>\d+)(?<unit>cm|in)$/.match(v)) &&
+        HGT_VALUES[m[:unit]].include?(m[:height].to_i)
+    },
+    'hcl' => ->(v) { /^\#{1}[0-9a-f]{6}$/ =~ v },
+    'ecl' => ->(v) { ECL_VALUES.include?(v) },
+    'pid' => ->(v) { /^\d{9}$/ =~ v },
+    'cid' => ->(_v) { true }
+  }.freeze
+
+  def self.parse_passports(input)
+    input.split(/^$/).map do |passport_section|
       passport_section.split(/\s/)
                       .reject(&:empty?)
                       .map { |kv| kv.split(':') }
                       .each_with_object({}) { |(key, value), hash| hash[key] = value }
     end
-
-    passports.count { |passport| valid_passport?(passport) }
   end
 
-  REQUIRED_FIELDS = %w[ecl pid eyr hcl byr iyr hgt].freeze
+  def self.count_passports_with_required_fields(input)
+    parse_passports(input).count { |passport| required_fields?(passport.keys) }
+  end
 
-  def self.valid_passport?(passport)
-    (REQUIRED_FIELDS - passport.keys).empty?
+  def self.required_fields?(fields)
+    (REQUIRED_FIELDS - fields).empty?
+  end
+
+  def self.count_valid_passports(input)
+    parse_passports(input).count do |passport|
+      required_fields?(passport.keys) &&
+        passport.all? { |field, value| VALIDATORS[field].call(value) }
+    end
   end
 end
--- spec/day4/passport_processing_spec.rb
+++ spec/day4/passport_processing_spec.rb
@@ -1,17 +1,42 @@
 # frozen_string_literal: true
 
 RSpec.describe PassportProcessing do
-  context 'when example data is used' do
-    let(:input) { File.read(File.join(__dir__, 'input1')) }
-    it 'counts to 2' do
-      expect(described_class.count_valid(input)).to eq(2)
+  describe 'part 1' do
+    context 'when example data is used' do
+      let(:input) { File.read(File.join(__dir__, 'input1')) }
+      it 'counts to 2' do
+        expect(described_class.count_passports_with_required_fields(input)).to eq(2)
+      end
+    end
+
+    context 'when real data is used' do
+      let(:input) { File.read(File.join(__dir__, 'input2')) }
+      it 'counts to 170' do
+        expect(described_class.count_passports_with_required_fields(input)).to eq(170)
+      end
     end
   end
 
-  context 'when real data is used' do
-    let(:input) { File.read(File.join(__dir__, 'input2')) }
-    it 'counts to 170' do
-      expect(described_class.count_valid(input)).to eq(170)
+  describe 'part 2' do
+    context 'when invalid passports given' do
+      let(:input) { File.read(File.join(__dir__, 'invalid_passports')) }
+      it 'counts to 2' do
+        expect(described_class.count_valid_passports(input)).to eq(0)
+      end
+    end
+
+    context 'when valid passports given' do
+      let(:input) { File.read(File.join(__dir__, 'valid_passports')) }
+      it 'counts to 170' do
+        expect(described_class.count_valid_passports(input)).to eq(4)
+      end
+    end
+
+    context 'when problem data given' do
+      let(:input) { File.read(File.join(__dir__, 'input2')) }
+      it 'counts to 170' do
+        expect(described_class.count_valid_passports(input)).to eq(103)
+      end
     end
   end
 end
