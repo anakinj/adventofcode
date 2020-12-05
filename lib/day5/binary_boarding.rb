c16d4967312b9d9a617063bc5ae20728fac51ede
Joakim Antman <antmanj@gmail.com>
2020-12-05 13:29:02 +0200

    Day5 part 1

---
+++ lib/day5/binary_boarding.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module BinaryBoarding
+  def self.max_seat_id(input)
+    seat_ids = process_boarding_passes(input).map { |seat| seat[2] }
+    seat_ids.max
+  end
+
+  def self.process_boarding_passes(input)
+    input.each_line.map(&:chomp).map do |pass|
+      process_boarding_pass(pass)
+    end
+  end
+
+  def self.process_boarding_pass(boarding_pass)
+    row  = find_one((0..127).to_a, path: boarding_pass[0..6], left: 'F', right: 'B')
+    seat = find_one((0..7).to_a, path: boarding_pass[7..9], left: 'L', right: 'R')
+    id   = row * 8 + seat
+
+    [row, seat, id]
+  end
+
+  def self.find_one(arr, path:, left:, right:)
+    path.each_char do |char|
+      middle = arr.size / 2
+      arr = if char == left
+              (arr.first..arr[middle - 1]).to_a
+            elsif char == right
+              (arr[middle]..arr.last).to_a
+            end
+    end
+
+    arr.first
+  end
+end
---
+++ spec/day5/binary_boarding_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+RSpec.describe BinaryBoarding do
+  let(:input) { File.read(File.join(__dir__, 'input1')) }
+
+  context 'when example boardingpasses given' do
+    it 'figures out row 44, seat 5 and id 357' do
+      expect(described_class.process_boarding_pass('FBFBBFFRLR')).to eq([44, 5, 357])
+    end
+
+    it 'figures out row 44, seat 5 and id 357' do
+      expect(described_class.process_boarding_pass('BFFFBBFRRR')).to eq([70, 7, 567])
+    end
+
+    it 'figures out row 44, seat 5 and id 357' do
+      expect(described_class.process_boarding_pass('FFFBBBFRRR')).to eq([14, 7, 119])
+    end
+
+    it 'figures out row 44, seat 5 and id 357' do
+      expect(described_class.process_boarding_pass('BBFFBBFRLL')).to eq([102, 4, 820])
+    end
+  end
+
+  context 'when big set is given' do
+    it 'figures out the biggest seat id' do
+      expect(described_class.max_seat_id(input)).to eq(908)
+    end
+  end
+end
