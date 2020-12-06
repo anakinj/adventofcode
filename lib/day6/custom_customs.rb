36f2a1a56832631542a37b8d184a84e710849484
Joakim Antman <antmanj@gmail.com>
2020-12-06 21:33:24 +0200

    Day 6 Part 2

--- lib/day6/custom_customs.rb
+++ lib/day6/custom_customs.rb
@@ -6,4 +6,10 @@ module CustomCustoms
       group.split("\n").reject(&:empty?).inject(&:+).chars.uniq.size
     end.inject(&:+)
   end
+
+  def self.count_same_answers(input)
+    input.split(/^$/).map do |group|
+      group.split("\n").reject(&:empty?).map(&:chars).inject(:&).size
+    end.inject(&:+)
+  end
 end
--- spec/day6/custom_customs_spec.rb
+++ spec/day6/custom_customs_spec.rb
@@ -4,15 +4,30 @@ RSpec.describe CustomCustoms do
   let(:example) { File.read(File.join(__dir__, 'example')) }
   let(:input) { File.read(File.join(__dir__, 'input1')) }
 
-  context 'when example is given' do
-    it 'counts to 11' do
-      expect(described_class.count_answers(example)).to eq(11)
+  describe '.count_answers' do
+    context 'when example is given' do
+      it 'counts to 11' do
+        expect(described_class.count_answers(example)).to eq(11)
+      end
+    end
+
+    context 'when real input is given' do
+      it 'counts to 6437 unique answers' do
+        expect(described_class.count_answers(input)).to eq(6437)
+      end
     end
   end
 
-  context 'when example is given' do
-    it 'counts to 6437' do
-      expect(described_class.count_answers(input)).to eq(6437)
+  describe '.count_same_answers' do
+    context 'when example is given' do
+      it 'counts to 11' do
+        expect(described_class.count_same_answers(example)).to eq(6)
+      end
+    end
+    context 'when real input is given' do
+      it 'counts to 6437 unique answers' do
+        expect(described_class.count_same_answers(input)).to eq(3229)
+      end
     end
   end
 end
