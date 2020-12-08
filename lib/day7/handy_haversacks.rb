0c214a39768d4ccd6fb45265e3348a4785c225c9
Joakim Antman <antmanj@gmail.com>
2020-12-08 08:10:54 +0200

    A little cleanup on yesterdays solution

--- lib/day7/handy_haversacks.rb
+++ lib/day7/handy_haversacks.rb
@@ -15,6 +15,15 @@ module HandyHaversacks
     parents.size
   end
 
+  def self.iterate_parents(bag, &block)
+    return if bag.nil?
+
+    bag[:parents].each do |parent|
+      yield(parent)
+      iterate_parents(parent, &block)
+    end
+  end
+
   def self.count_totals(input, color)
     bags = parse_bags(input)
     count_children(bags[color][:children])
@@ -27,36 +36,33 @@ module HandyHaversacks
   end
 
   def self.parse_bags(input)
-    bags = {}
-    input.each_line do |line|
-      match = BAG_REGEX.match(line)
-      match[:content].split(',')
-                     .map(&:strip)
-                     .map { |part| CONTENT_REGEX.match(part) }
-                     .each do |bag|
-                       container = bags[match[:color]] ||= {
-                         color: match[:color],
-                         parents: [],
-                         children: {}
-                       }
-                       content = bags[bag[:color]] ||= {
-                         color: bag[:color],
-                         parents: [],
-                         children: {}
-                       }
-                       content[:parents] << container
-                       container[:children][content] = (bag[:count] || 1).to_i unless bag[:color] == NO_OTHER
-                     end
+    parse_raw_bag_data(input).each_with_object({}) do |raw_bag, bags|
+      parse_bag_content(raw_bag[:content]).each do |bag|
+        container = bags[raw_bag[:color]] ||= create_bag(raw_bag[:color])
+        content = bags[bag[:color]] ||= create_bag(bag[:color])
+        content[:parents] << container
+        next if bag[:color] == NO_OTHER
+
+        container[:children][content] = (bag[:count] || 1).to_i
+      end
     end
-    bags
   end
 
-  def self.iterate_parents(bag, &block)
-    return if bag.nil?
+  def self.parse_raw_bag_data(input)
+    input.each_line.map { |line| BAG_REGEX.match(line) }
+  end
 
-    bag[:parents].each do |parent|
-      yield(parent)
-      iterate_parents(parent, &block)
-    end
+  def self.parse_bag_content(content)
+    content.split(',')
+           .map(&:strip)
+           .map { |part| CONTENT_REGEX.match(part) }
+  end
+
+  def self.create_bag(color)
+    {
+      color: color,
+      parents: [],
+      children: {}
+    }
   end
 end
