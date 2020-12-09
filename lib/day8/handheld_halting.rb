3320ed3bbe313966814f987f1e878266ae8e9cb4
Joakim Antman <antmanj@gmail.com>
2020-12-09 08:20:00 +0200

    Cleaning up the solution

--- lib/day8/handheld_halting.rb
+++ lib/day8/handheld_halting.rb
@@ -1,46 +1,16 @@
 # frozen_string_literal: true
 
 module HandheldHalting
-  class Bootloader
-    SWAPPABLE = %w[jmp nop].freeze
+  class Interpreter
     def initialize(instructions)
-      @instructions = instructions.each_line.map { |l| l.split(' ') }
+      @instructions = instructions
       @acc = 0
       @pos = 0
       @stack = []
-    end
-
-    def fix_and_execute
-      fix_pos = 0
-      original_instructions = @instructions.dup
-      begin
-        execute!
-      rescue StandardError
-        @instructions = original_instructions.dup
-        while ((inst, val) = @instructions[fix_pos += 1])
-          case inst
-          when 'jmp'
-            @instructions[fix_pos] = ['nop', val]
-          when 'nop'
-            @instructions[fix_pos] = ['jmp', val]
-          else
-            next
-          end
-
-          break
-        end
-        retry
-      end
-      @acc
     end
 
     def execute!
-      @acc = 0
-      @pos = 0
-      @stack = []
       while ((inst, val) = @instructions[@pos])
-        # p "#{@pos}: #{inst} #{val} #{@acc} #{@stack.size}"
-
         raise 'Infinite loop detected' if @stack.include?(@pos)
 
         @stack << @pos
@@ -58,17 +28,62 @@ module HandheldHalting
 
     private
 
-    def nop(_val)
+    def nop(*)
       1
     end
 
     def acc(val)
-      @acc += val.to_i
+      @acc += val
       1
     end
 
     def jmp(val)
-      val.to_i
+      val
+    end
+  end
+
+  class Fixer
+    SWAPPABLE = {
+      'jmp' => 'nop',
+      'nop' => 'jmp'
+    }.freeze
+
+    def initialize(instructions)
+      @instructions = instructions
+      @fix_pos = 0
+    end
+
+    def while_fixing
+      yield(fix_instruction(@instructions.dup))
+    rescue StandardError
+      retry
+    end
+
+    def fix_instruction(instructions)
+      while ((inst, val) = instructions[@fix_pos += 1])
+        next unless (new_instruction = SWAPPABLE[inst])
+
+        instructions[@fix_pos] = [new_instruction, val]
+        break
+      end
+      instructions
+    end
+  end
+
+  def self.fix_and_execute(instructions)
+    Fixer.new(parse_instructions(instructions)).while_fixing do |fixed_instructions|
+      Interpreter.new(fixed_instructions).execute!
+    end
+  end
+
+  def self.execute(instructions)
+    Interpreter.new(parse_instructions(instructions)).execute
+  end
+
+  def self.parse_instructions(instructions)
+    instructions.each_line.map do |l|
+      inst, val = l.split(' ')
+      [inst, val.to_i]
     end
   end
 end
--- spec/day8/handheld_halting_spec.rb
+++ spec/day8/handheld_halting_spec.rb
@@ -1,31 +1,30 @@
 # frozen_string_literal: true
 
 RSpec.describe HandheldHalting do
-  let(:bootloader) { HandheldHalting::Bootloader.new(input) }
   context 'when example is given' do
     let(:input) { read_file('day8/example') }
     it 'counts to 5' do
-      expect(bootloader.execute).to eq(5)
+      expect(described_class.execute(input)).to eq(5)
     end
   end
   context 'when actual data is given' do
     let(:input) { read_file('day8/input') }
     it 'counts to 1594' do
-      expect(bootloader.execute).to eq(1594)
+      expect(described_class.execute(input)).to eq(1594)
     end
   end
 
   context 'when example is asked to fix itself' do
     let(:input) { read_file('day8/example') }
     it 'counts to 8' do
-      expect(bootloader.fix_and_execute).to eq(8)
+      expect(described_class.fix_and_execute(input)).to eq(8)
     end
   end
 
   context 'when actual data is asked to fix itself' do
     let(:input) { read_file('day8/input') }
     it 'counts to 758' do
-      expect(bootloader.fix_and_execute).to eq(758)
+      expect(described_class.fix_and_execute(input)).to eq(758)
     end
   end
 end
