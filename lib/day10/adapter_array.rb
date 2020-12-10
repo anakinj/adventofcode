0deb590a2a6c42e24e0c3b195537fdc14e509d52
Joakim Antman <antmanj@gmail.com>
2020-12-11 00:06:24 +0200

    Day 10 part 2

--- lib/day10/adapter_array.rb
+++ lib/day10/adapter_array.rb
@@ -1,15 +1,44 @@
 # frozen_string_literal: true
 
 module AdapterArray
-  def self.differences_multiplied(input)
+  def self.adapters_from_input(input)
     adapters = input.each_line.map(&:to_i).sort
+    [0] + adapters + [adapters.max + 3]
+  end
 
-    adapters = [0] + adapters + [adapters.max + 3]
-
+  def self.differences_multiplied(input)
+    adapters = adapters_from_input(input)
     differences = (0..adapters.size - 2).each_with_object([]) do |adapter_index, diffs|
       diffs << adapters[adapter_index + 1] - adapters[adapter_index]
     end
 
     differences.count(1) * differences.count(3)
+  end
+
+  def self.count_combos(input)
+    ComboCounter.new(adapters_from_input(input)).combination_count
+  end
+
+  class ComboCounter
+    attr_reader :adapters, :paths_taken
+
+    def initialize(adapters)
+      @adapters = adapters
+      @paths_taken = {}
+    end
+
+    def combination_count
+      count_combos(adapters.min, adapters.max)
+    end
+
+    def count_combos(current_adapter, end_adapter)
+      return paths_taken[current_adapter] if paths_taken.include?(current_adapter)
+      return 1 if current_adapter == end_adapter
+
+      paths_taken[current_adapter] = (1..3).map { |step| current_adapter + step }
+                                           .select { |possible_adapter| adapters.include?(possible_adapter) }
+                                           .map { |validated_adapter| count_combos(validated_adapter, end_adapter) }
+                                           .sum
+    end
   end
 end
--- spec/day10/adapter_array_spec.rb
+++ spec/day10/adapter_array_spec.rb
@@ -2,23 +2,34 @@
 
 RSpec.describe AdapterArray do
   describe '.differences_multiplied' do
+    subject { described_class.differences_multiplied(input) }
     context 'when example is given' do
       let(:input) { read_file('day10/example1') }
-      it 'returns 35' do
-        expect(described_class.differences_multiplied(input)).to eq(35)
-      end
+      it { is_expected.to eq(35) }
     end
     context 'when second example is given' do
       let(:input) { read_file('day10/example2') }
-      it 'returns 220' do
-        expect(described_class.differences_multiplied(input)).to eq(220)
-      end
+      it { is_expected.to eq(220) }
     end
     context 'when data is given' do
       let(:input) { read_file('day10/input') }
-      it 'returns 2376' do
-        expect(described_class.differences_multiplied(input)).to eq(2376)
-      end
+      it { is_expected.to eq(2376) }
+    end
+  end
+
+  describe '.count_combos' do
+    subject { described_class.count_combos(input) }
+    context 'when example is given' do
+      let(:input) { read_file('day10/example1') }
+      it { is_expected.to eq(8) }
+    end
+    context 'when second example is given' do
+      let(:input) { read_file('day10/example2') }
+      it { is_expected.to eq(19_208) }
+    end
+    context 'when data is given' do
+      let(:input) { read_file('day10/input') }
+      it { is_expected.to eq(129_586_085_429_248) }
     end
   end
 end
