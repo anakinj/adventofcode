e52c523d01f55030d6e13e7285fe74d1fe812cad
Joakim Antman <antmanj@gmail.com>
2020-12-13 12:38:00 +0200

    Day13 part 2

--- lib/day13/shuttle_search.rb
+++ lib/day13/shuttle_search.rb
@@ -13,4 +13,39 @@ module ShuttleSearch
     earliest_departure_time = schedules.values.min
     schedules.key(earliest_departure_time) * (earliest_departure_time - now)
   end
+
+  def self.coin_contest_answer(input) # rubocop:disable Metrics/MethodLength
+    busses       = parse_and_sort_busses(input)
+    first_bus    = busses.first
+    time_step    = first_bus[:number]
+    current_time = first_bus[:number] - first_bus[:pos]
+
+    busses.each do |bus|
+      current_time += time_step until valid_time?(current_time, bus)
+
+      subset_time = current_time + time_step
+      subset      = busses[0..(busses.index(bus))]
+
+      subset_time += time_step until subset.all? { |sub_bus| valid_time?(subset_time, sub_bus) }
+
+      time_step = subset_time - current_time
+    end
+
+    current_time
+  end
+
+  def self.parse_and_sort_busses(input)
+    busses = input.each_line.to_a.last.split(',')
+                  .map.with_index.each_with_object([]) do |(bus, pos), obj|
+      next if bus == 'x'
+
+      obj << { number: bus.to_i, pos: pos }
+    end
+
+    busses.sort_by { |v| -v[:number] }
+  end
+
+  def self.valid_time?(time, bus)
+    ((time + bus[:pos]) % bus[:number]).zero?
+  end
 end
--- spec/day13/shuttle_search_spec.rb
+++ spec/day13/shuttle_search_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe ShuttleSearch do
       it { is_expected.to eq(246) }
     end
   end
+
+  describe '.coin_contest_answer' do
+    subject { described_class.coin_contest_answer(input) }
+    context 'when example is given' do
+      let(:input) { read_file('day13/example1') }
+      it { is_expected.to eq(1_068_781) }
+    end
+    context 'when input is given' do
+      let(:input) { read_file('day13/input') }
+      it { is_expected.to eq(939_490_236_001_473) }
+    end
+  end
 end
