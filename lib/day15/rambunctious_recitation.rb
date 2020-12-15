6b3d91a7d0be69c0c77e20f39b6de2286c449139
Joakim Antman <antmanj@gmail.com>
2020-12-15 21:47:15 +0200

    Minor refactor

--- lib/day15/rambunctious_recitation.rb
+++ lib/day15/rambunctious_recitation.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class RambunctiousRecitation
-  attr_reader :starting_numbers
+  attr_reader :starting_numbers, :spoken_numbers
 
   def initialize(starting_numbers)
     @starting_numbers = starting_numbers.split(',').map(&:to_i)
+    @spoken_numbers = {}
   end
 
   def speak_until(turns: 2020)
-    spoken_numbers = {}
     last_number = nil
 
     starting_numbers.each_with_index do |n, i|
@@ -17,14 +17,18 @@ class RambunctiousRecitation
     end
 
     (starting_numbers.size + 1..turns).each do |turn|
-      last_number = if Array(spoken_numbers[last_number]).size > 1
-                      spoken_numbers[last_number][-1] - spoken_numbers[last_number][-2]
-                    else
-                      0
-                    end
+      last_number = next_number(last_number)
       (spoken_numbers[last_number] ||= []) << turn
     end
 
     last_number
+  end
+
+  def next_number(last_number)
+    if (prev_numbers = Array(spoken_numbers[last_number])).size > 1
+      prev_numbers[-1] - prev_numbers[-2]
+    else
+      0
+    end
   end
 end
