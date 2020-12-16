dc74408e1fb22de46e55d789f9626b2d9536b379
Joakim Antman <antmanj@gmail.com>
2020-12-16 22:31:29 +0200

    Day 16 part 2

--- lib/day16/ticket_translation.rb
+++ lib/day16/ticket_translation.rb
@@ -3,8 +3,11 @@
 class TicketTranslation
   class Rule
     RULE_REGEX = /^(?<field>.*): (?<start1>\d+)-(?<end1>\d+) or (?<start2>\d+)-(?<end2>\d+)$/.freeze
+    attr_reader :field
+
     def initialize(rule_data)
       rule_match = rule_data.match(RULE_REGEX)
+      @field  = rule_match[:field]
       @ranges = [rule_match[:start1].to_i..rule_match[:end1].to_i,
                  rule_match[:start2].to_i..rule_match[:end2].to_i]
     end
@@ -19,21 +22,71 @@ class TicketTranslation
       groups.split("\n")
             .reject(&:empty?)
     end
-    [@raw_my_ticket, @raw_nearby_tickets].each(&:shift)
-  end
-
-  def rules
-    @rules ||= @raw_rules.map { |r| Rule.new(r) }
-  end
-
-  def tickets
-    @tickets ||= @raw_nearby_tickets.map { |t| t.split(',').map(&:to_i) }
+    [@raw_my_ticket,
+     @raw_nearby_tickets].each(&:shift)
   end
 
   def error_rate
     tickets.map do |ticket|
       ticket.reject { |v| one_valid_rule?(v) }
     end.flatten.sum
+  end
+
+  def fields_multiplied(field_matcher = /^departure/)
+    indexes = figure_out_positions.each_with_object([]) do |(k, v), carry|
+      next unless field_matcher.match(k)
+
+      carry << v.first
+    end
+
+    indexes.map do |i|
+      my_ticket[i]
+    end.inject(&:*)
+  end
+
+  private
+
+  def figure_out_positions
+    matches = rule_matches
+
+    until matches.values.all? { |v| v.size == 1 }
+      singles = matches.values.select { |v| v.size == 1 }.map(&:first)
+
+      matches.each_value do |v|
+        next if v.size == 1
+
+        singles.each { |s| v.delete(s) }
+      end
+    end
+    matches
+  end
+
+  def rule_matches
+    rules.each_with_object({}) do |rule, coll|
+      matches = (0..my_ticket.size).select do |field_index|
+        values = (valid_tickets + [my_ticket]).map { |t| t[field_index] }
+        true if values.all? { |value| rule.valid?(value) }
+      end
+      coll[rule.field] = matches
+    end
+  end
+
+  def valid_tickets
+    @valid_tickets ||= tickets.select do |ticket|
+      ticket.all? { |v| one_valid_rule?(v) }
+    end
+  end
+
+  def rules
+    @rules ||= @raw_rules.map { |r| Rule.new(r) }
+  end
+
+  def my_ticket
+    @my_ticket ||= @raw_my_ticket.first.split(',').map(&:to_i)
+  end
+
+  def tickets
+    @tickets ||= @raw_nearby_tickets.map { |t| t.split(',').map(&:to_i) }
   end
 
   def one_valid_rule?(value)
--- spec/day16/ticket_translation_spec.rb
+++ spec/day16/ticket_translation_spec.rb
@@ -12,4 +12,17 @@ RSpec.describe TicketTranslation do
       it { is_expected.to eq(26_053) }
     end
   end
+
+  describe '.error_rate' do
+    subject { described_class.new(input).fields_multiplied }
+    context 'when example is given' do
+      subject { described_class.new(input).fields_multiplied(/s/) }
+      let(:input) { read_file('day16/example2') }
+      it { is_expected.to eq(12 * 13) }
+    end
+    context 'when input is given' do
+      let(:input) { read_file('day16/input') }
+      it { is_expected.to eq(1_515_506_256_421) }
+    end
+  end
 end
