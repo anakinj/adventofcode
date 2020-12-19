0eed8c9867625bc1b1b778dcea6ae529f7c76adf
Joakim Antman <antmanj@gmail.com>
2020-12-19 23:54:24 +0200

    Day 19 part 2

--- lib/day19/monster_messages.rb
+++ lib/day19/monster_messages.rb
@@ -11,6 +11,19 @@ module MonsterMessages
     messages.each_line.count { |m| rule_regex.match(m) }
   end
 
+  def self.valid_message_count_with_replacement(input)
+    rules, messages = input.split(/^$/)
+    parsed_rules = parse_rules(rules)
+    replace_rules(parsed_rules)
+    rule_regex = Regexp.new("^#{build_rule(parsed_rules, '0')}$")
+    messages.each_line.count { |m| rule_regex.match(m) }
+  end
+
+  def self.replace_rules(rules)
+    rules['8'] = '42 | 42 8'
+    rules['11'] = '42 31 | 42 11 31'
+  end
+
   def self.parse_rules(rules)
     rules.each_line.each_with_object({}) do |r, h|
       m = RULE_REGEX.match(r)
@@ -18,12 +31,14 @@ module MonsterMessages
     end
   end
 
-  def self.build_rule(rules, start)
+  def self.build_rule(rules, start, depth = 0)
+    return '' if depth > 15
+
     rule = rules[start]
     combined = []
     rule.split(' ').each do |part|
       combined << if part =~ /\d+/
-                    "(#{build_rule(rules, part)})"
+                    "(#{build_rule(rules, part, depth + 1)})"
                   else
                     part
                   end
--- spec/day19/monster_message_spec.rb
+++ spec/day19/monster_message_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe MonsterMessages do
       it { is_expected.to eq(248) }
     end
   end
+
+  describe '.valid_message_count_with_replacement' do
+    subject { described_class.valid_message_count_with_replacement(input) }
+    context 'when example is given' do
+      let(:input) { read_file('day19/example2') }
+      it { is_expected.to eq(12) }
+    end
+
+    context 'when input is given' do
+      let(:input) { read_file('day19/input') }
+      it { is_expected.to eq(381) }
+    end
+  end
 end
