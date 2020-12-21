f021e5c538af5bf9b0b1f63a9825abd54ad1e487
Joakim Antman <antmanj@gmail.com>
2020-12-21 08:16:40 +0200

    Day 20 part 2 in the making

--- lib/day20/jurassic_jigsaw.rb
+++ lib/day20/jurassic_jigsaw.rb
@@ -2,28 +2,65 @@
 
 module JurassicJigsaw
   class Tile
-    attr_reader :id, :top, :left, :right, :bottom, :matches
+    attr_reader :id, :top, :left, :right, :bottom, :matches, :content
 
     ID_REGEX = /Tile (?<id>\d+):/.freeze
-    def initialize(tile_input)
-      @lines = []
-      @left = []
-      @right = []
-      tile_input.each_line.map(&:chomp).each do |line|
+
+    def to_s
+      tile_input
+    end
+
+    def initialize(tile_input, id = nil)
+      @tile_input = tile_input
+      @lines   = []
+      @left    = []
+      @right   = []
+      @content = []
+      @id      = id
+
+      tile_input.each do |line|
         next if line.empty?
 
         if (match = ID_REGEX.match(line))
           @id = match[:id].to_i
         else
           @top ||= line
-          @left << line.chars[0]
-          @right << line.chars[-1]
-          @lines << line
-          @bottom = line
+          @left    << line.chars[0]
+          @content << line.chars
+          @right   << line.chars[-1]
+          @lines   << line
+          @bottom  = line
         end
       end
-      @left = @left.join
+
+      @left  = @left.join
       @right = @right.join
+    end
+
+    def variants
+      variants = [rotate(@content)]
+      [@content, rotate(@content)].each do |content|
+        variants << self.class.new(content.reverse.map(&:join), id)
+        variants << self.class.new(content.reverse.map(&:reverse).map(&:join), id)
+        variants << self.class.new(content.map(&:reverse).map(&:join), id)
+      end
+    end
+
+    def rotate(content)
+      (0...content.length).map { |i| content.map { |row| row[i] } }
+    end
+
+    def neighbour(on)
+      case on
+      when :left
+        @matches[left]
+      when :right
+        @matches[right]
+      when :top
+        @matches[top]
+      when :bottom
+        @matches[bottom]
+      end
     end
 
     def combinations
@@ -58,10 +95,33 @@ module JurassicJigsaw
   end
 
   def self.multiply_corners(input)
-    tiles = input.split(/^$/).map do |tile_input|
+    tiles = parse_tiles(input.each_line.map(&:chomp))
+
+    corners, = resolve_tiles(tiles)
+
+    corners.map(&:id).inject(&:*)
+  end
+
+  def self.seamonster(input)
+    tiles = parse_tiles(input.each_line.map(&:chomp))
+
+    corners, borders, content = resolve_tiles(tiles)
+
+    top_left     = corners.select { |c| c.neighbour(:right) && c.neighbour(:bottom) }
+    top_right    = corners.select { |c| c.neighbour(:left) && c.neighbour(:bottom) }
+
+    bottom_right = corners.select { |c| c.neighbour(:left) && c.neighbour(:top) }
+    bottom_left  = corners.select { |c| c.neighbour(:right) && c.neighbour(:top) }
+    binding.irb
+  end
+
+  def self.parse_tiles(input)
+    input.split(/^$/).map do |tile_input|
       Tile.new(tile_input)
     end
+  end
 
+  def self.resolve_tiles(tiles)
     lookup = {}
 
     tiles.each do |tile|
@@ -72,8 +132,20 @@ module JurassicJigsaw
       tile.resolve_matches(lookup)
     end
 
-    tiles.select do |border_tile|
-      border_tile.matches.count == 4
-    end.map(&:id).inject(&:*)
+    corner_tiles = []
+    border_tiles = []
+    content_tiles = []
+
+    tiles.each do |tile|
+      case tile.matches.count
+      when 4
+        corner_tiles << tile
+      when 6
+        border_tiles << tile
+      else
+        content_tiles << tile
+      end
+    end
+    [corner_tiles, border_tiles, content_tiles]
   end
 end
--- spec/day20/jurrassic_jigsaw_spec.rb
+++ spec/day20/jurrassic_jigsaw_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe JurassicJigsaw do
       it { is_expected.to eq(104_831_106_565_027) }
     end
   end
+
+  describe '.seamonster' do
+    subject { described_class.seamonster(input) }
+    context 'when example is given' do
+      let(:input) { read_file('day20/example') }
+      it { is_expected.to eq(273) }
+    end
+
+    context 'when input is given' do
+      let(:input) { read_file('day20/input') }
+      it { is_expected.to eq(104_831_106_565_027) }
+    end
+  end
 end
